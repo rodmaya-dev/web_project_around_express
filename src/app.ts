ed1762a42a791b52cd61cbca7496d337ab106769
Rodrigo Maya <rodrigomaya000@gmail.com>
2026-09-01 16:24:44 -0600

    chore: setup database connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 import mainRouter from "./routes/index.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/aroundb") // aroundb es la base de datos que usaremos
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
